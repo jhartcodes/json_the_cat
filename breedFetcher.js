e7fa873be1ce55a9bfff46d4814ab5abefd598e4
Joel <joel.d.hart1@gmail.com>
2021-03-12 05:06:36 +0000

    added test file and index to run the breedtester

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,24 @@
 const request = require('request');
-const breed = process.argv.slice(2);
-
-const breedFetcher = function(breed) {
-  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
   request(url,(error, response, body) => {
     if (error) {
       console.log(response,'you need a cat breed');
-    } else if (body.length < 4) {
-      console.log('there are no cats silly');
     } else {
       const data = JSON.parse(body);
-      console.log(data[0].description);
+      const breedName = data[0];
+      console.log(breedName);
+      if (breedName) {
+        callback(null,breedName.description);
+      } else {
+        callback('This breed is not found');
+      }
     }
   });
 };
 
-breedFetcher(breed);
+module.exports = { fetchBreedDescription };
 // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
 
 // console.log('body:', body); // Print the HTML for the Google homepage.
